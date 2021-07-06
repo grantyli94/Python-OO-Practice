3f23d06ce2f73131f64482275fc2e2804254dcb6
Grant <grantyli94@gmail.com>
2021-07-06 18:40:40 -0400

    SerialGenerator and WordFinder

--- serial.py
+++ serial.py
@@ -17,3 +17,16 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start=100):
+        """ initiate a serial generator with a passed in start value """
+        self.start = start
+        self.count = -1
+    
+    def generate(self):
+        """ generates the next sequential number """
+        self.count += 1
+        return self.start + self.count
+
+    def reset(self):
+        """ resets the serial generator to the original start value """
+        self.count = -1
--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,15 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_path):
+        self.f = open(file_path,"r")
+        self.words = self.f.read().split("\n")
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        return choice(self.words)
+        
+        
+    
+    
