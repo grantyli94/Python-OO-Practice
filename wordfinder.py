f28a0769dfd5b55f58ac6782a2d119255fb8b4c7
Mike Chang <mchang95@gmail.com>
2021-07-06 17:07:31 -0700

    adds randomwordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,15 +1,30 @@
 from random import choice
 
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: creates list of words from reference file."""
+
     def __init__(self, file_path):
-        self.f = open(file_path,"r")
-        self.words = self.f.read().split("\n")
+        """ initiates word finder with file path """
+        file = open(file_path,"r")
+        self.words = self.read_file(file)
         print(f"{len(self.words)} words read")
 
     def random(self):
+        """ returns a random word from word list """
         return choice(self.words)
-        
-        
-    
-    
+
+    def read_file(self, file):
+        """ returns list of words from reference file """
+        return file.read().splitlines()
+
+
+
+
+class RandomWordFinder(WordFinder): 
+    """RandomWordFinder: filters out empty entires and comments"""
+
+    def read_file(self, file):
+        """ returns list of words from file without empty entries or comments """
+        #replace ele with word
+        return [ele for ele in file.read().splitlines()
+            if ele != "" and "#" not in ele]
